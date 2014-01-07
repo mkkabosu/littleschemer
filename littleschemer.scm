362117677171cc47f6e8639fc71c93909ded8c38
makkabosu <lambddri@gmail.com>
2014-01-07 20:00:04 +0900

    atom?を追加

--- littleschemer.scm
+++ littleschemer.scm
@@ -1,3 +1,6 @@
+(define atom?
+  (lambda (x)
+    (and (not (pair? x)) (not (null? x)))))
 (define double
   (lambda (x)
     (* x 2))) 
@@ -8,4 +11,3 @@
      ((null? l) #t)
      ((atom? (car l)) (lat? (cdr l)))
      (else #f))))
-
