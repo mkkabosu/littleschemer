147cde3c32ce0be29d269124bb326582256c3b38
makkabosu <lambddri@gmail.com>
2014-01-07 23:57:50 +0900

    member?を追加

--- littleschemer.scm
+++ littleschemer.scm
@@ -1,6 +1,7 @@
 (define atom?
   (lambda (x)
     (and (not (pair? x)) (not (null? x)))))
+
 (define double
   (lambda (x)
     (* x 2))) 
@@ -11,3 +12,12 @@
      ((null? l) #t)
      ((atom? (car l)) (lat? (cdr l)))
      (else #f))))
+
+(define nil #f)
+
+(define member?
+  (lambda (a lat)
+    (cond
+     ((null? lat) nil)
+     (else (or (eq? (car lat) a)
+	       (member? a (cdr lat)))))))
