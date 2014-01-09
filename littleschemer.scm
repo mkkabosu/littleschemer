d8cfd1e8d9d3bc1e0f007a72568aabcf4bb3c933
makkabosu <lambddri@gmail.com>
2014-01-09 23:02:08 +0900

    関数remberの追加

--- littleschemer.scm
+++ littleschemer.scm
@@ -21,3 +21,11 @@
      ((null? lat) nil)
      (else (or (eq? (car lat) a)
 	       (member? a (cdr lat)))))))
+
+(define rember
+  (lambda (a lat)
+    (cond
+     ((null? lat)(quote ()))
+     ((eq? (car lat) a) (cdr lat))
+     (else (cons (car lat)
+		 (rember a (cdr lat)))))))
