2fffb0c9e5d2043e976b1d1bf3af3fbda7adad20
makkabosu <lambddri@gmail.com>
2014-01-12 01:21:32 +0900

    insertR insertLを追加

--- littleschemer.scm
+++ littleschemer.scm
@@ -29,3 +29,34 @@
      ((eq? (car lat) a) (cdr lat))
      (else (cons (car lat)
 		 (rember a (cdr lat)))))))
+
+(define firsts
+  (lambda (l)
+    (cond
+     ((null? l) (quote ()))
+     (else (cons (car (car l))
+	    (firsts (cdr l)))))))
+
+(define insertR
+  (lambda (new old lat)
+    (cond
+     ((null? lat) (quote ()))
+     (else (cond 
+	    ((eq? (car lat) old) 
+	     (cons old
+		   (cons new (cdr lat))))
+	    (else (cons (car lat)
+			(insertR new old
+				 (cdr lat)))))))))
+
+(define insertL
+  (lambda (new old lat)
+    (cond
+     ((null? lat)(quote ()))
+     (else (cond
+	    ((eq? (car lat) old)
+	     (cons new
+		   (cons old (cdr lat))))
+	    (else (cons (car lat)
+			(insertL new old 
+				 (cdr lat)))))))))
