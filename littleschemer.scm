4af859c3fe0daad93f83fd9cee9ed43e07606b10
makkabosu <lambddri@gmail.com>
2014-01-19 01:35:19 +0900

    三章終了

--- littleschemer.scm
+++ littleschemer.scm
@@ -92,3 +92,45 @@
        (else (cons (car lat)
 		   (multirember a
 				(cdr lat)))))))))
+(define multiinsertR
+  (lambda (new old lat)
+    (cond
+     ((null? lat) (quote ()))
+    (else
+     (cond
+      ((eq? (car lat) old)
+       (cons (car lat)
+	     (cons new
+		   (multiinsertR new old
+				 (cdr lat)))))
+      (else (cons (car lat)
+		  (multiinsertR new old
+				(cdr lat)))))))))
+
+(define multiinsertL
+  (lambda (new old lat)
+    (cond 
+     ((null? lat) (quote ()))
+    (else
+     (cond
+      ((eq? (car lat) old)
+       (cons new
+	     (cons old
+		   (multiinsertL new old
+				 (cdr lat)))))
+      (else (cons (car lat)
+		  (multiinsertL new old
+				(cdr lat)))))))))
+
+(define multisubst
+  (lambda (new old lat)
+    (cond
+     ((null? lat) (quote ()))
+     (else (cond
+	    ((eq? (car lat) old)
+	     (cons new
+		   (multisubst new old 
+			       (cdr lat))))
+	   (else (cons (car lat)
+		       (multisubst new old
+				   (cdr lat)))))))))
