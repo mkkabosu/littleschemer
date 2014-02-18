2bc3945c29ce407fa9029a44c83863f85657eb41
makkabosu <lambddri@gmail.com>
2014-02-19 01:12:02 +0900

    added ~occur

--- littleschemer.scm
+++ littleschemer.scm
@@ -195,3 +195,74 @@
      ((> n m) #f)
      ((< n m) #f)
      (else #t))))
+
+(define expt
+  (lambda (n m)
+    (cond
+     ((zero? m) 1)
+     (else (x n (expt n (sub1 m)))))))
+
+(define quotient
+  (lambda (n m)
+    (cond
+     ((< n m) 0)
+     (else (add1 (quotient (o- n m) m))))))
+
+(define length
+  (lambda (lat)
+    (cond
+     ((null? lat) 0)
+     (else (add1 (length (cdr lat)))))))
+
+(define pick
+  (lambda (n lat)
+    (cond
+     ((zero? (sub1 n)) (car lat))
+     (else (pick (sub1 n) (cdr lat))))))
+
+(define rempick
+  (lambda (n lat)
+    (cond
+     ((zero? (sub1 n)) (cdr lat))
+     (else (cons (car lat)
+		 (rempick (sub1 n)
+			  (cdr lat)))))))
+
+(define no-nums
+  (lambda (lat)
+    (cond
+     ((null? lat) (quote ()))
+     (else (cond
+	    ((number? (car lat))
+	     (no-nums (cdr lat)))
+	    (else (cons (car lat)
+			(no-nums
+			 (cdr lat)))))))))
+
+(define all-nums
+  (lambda (lat)
+    (cond
+     ((null? lat) (quote ()))
+     ((number? (car lat))
+      (cons (car lat)
+	    (all-nums (cdr lat))))
+     (else (all-nums (cdr lat))))))
+
+(define eqan?
+  (lambda (a1 a2)
+    (cond
+     ((and (number? a1) (number? a2))
+      (= a1 a2))
+     ((or (number? a1) (number? a2)) #f)
+     (else (eq? a1 a2)))))
+
+(define occur
+  (lambda (a lat)
+    (cond
+     ((null? lat) 0)
+     (else
+      (cond
+       ((eq? (car lat) a)
+	(add1 (occur a (cdr lat))))
+       (else (occur a (cdr lat))))))))
+
