a502acdd28e3a6f3b1b10ef1c793697898a4761d
makkabosu <lambddri@gmail.com>
2014-03-10 01:12:43 +0900

    added rember* insertR*

--- littleschemer.scm
+++ littleschemer.scm
@@ -223,7 +223,7 @@
 (define rempick
   (lambda (n lat)
     (cond
-     ((zero? (sub1 n)) (cdr lat))
+     ((one? n) (cdr lat))
      (else (cons (car lat)
 		 (rempick (sub1 n)
 			  (cdr lat)))))))
@@ -266,3 +266,38 @@
 	(add1 (occur a (cdr lat))))
        (else (occur a (cdr lat))))))))
 
+(define one?
+  (lambda (n)
+    (= n 1)))
+
+(define rember*
+  (lambda (a l)
+    (cond
+     ((null? l) (quote ()))
+     ((atom? (car l))
+      (cond
+       ((eq? (car l) a)
+	(rember* a (cdr l)))
+       (else (cons (car l)
+		   (rember* a (cdr l))))))
+     (else (cons (rember* a (car l))
+		 (rember* a (cdr l)))))))
+
+(define insertR*
+  (lambda (new old l)
+    (cond
+     ((null? l) (quote ()))
+     ((atom? (car l))
+      (cond
+       ((eq? (car l) old)
+	(cons old
+	      (cons new
+		    (insertR* new old
+			      (cdr l)))))
+       (else (cons (car l)
+		   (insertR* new old
+			     (cdr l))))))
+     (else (cons (insertR* new old
+			   (car l))
+		 (insertR* new old
+			   (cdr l)))))))
