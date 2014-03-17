9c82ca603b41642e0469868d3375c0e0ff0fa0c3
makkabosu <lambddri@gmail.com>
2014-03-18 01:33:48 +0900

    added occur* and subst*

--- littleschemer.scm
+++ littleschemer.scm
@@ -301,3 +301,31 @@
 			   (car l))
 		 (insertR* new old
 			   (cdr l)))))))
+
+(define occur*
+  (lambda (a l)
+    (cond
+     ((null? l) 0)
+     ((atom? (car l))
+      (cond
+       ((eq? (car l) a)
+	(add1 (occur* a (cdr l))))
+       (else (occur* a (cdr l)))))
+      (else (o+ (occur* a (car l))
+		(occur* a (cdr l)))))))
+
+(define subst* 
+  (lambda (new old l)
+    (cond
+     ((null? l) (quote ()))
+     ((atom? (car l))
+      (cond
+       ((eq? (car l) old)
+	(cons new
+	      (subst* new old (cdr l))))
+       (else (cons (car l)
+		   (subst* new old
+			   (cdr l))))))
+     (else
+      (cons (subst* new old (car l))
+	    (subst* new old (cdr l)))))))
